43b66fdae633eb8f78e927e686ad233367b3edda
CEALMEBA <cesamendoza19@gmail.com>
2021-11-23 15:13:26 -0600

    api creada en .net

---
+++ LoginAPI/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginAPI.Models
+{
+    using System;
+    using System.Collections.Generic;
+    public class Department
+    {
+        public long DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
